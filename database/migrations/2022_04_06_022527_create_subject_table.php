ce8f820834ac4b0ace023a2dc8ea6514013906b8
Ho Wai Kit <68110992+1HoWK@users.noreply.github.com>
2022-04-06 11:26:45 +0800

    Wai Kit's db migration

---
+++ database/migrations/2022_04_06_022527_create_subject_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Subject', function (Blueprint $table) {
+            $table->increments('subjectID');
+            $table->string('courseCode');
+            $table->string('title');
+            $table->integer('pages');
+            $table->double('price', 8, 2);
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subject');
+    }
+};
