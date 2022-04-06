2cd9c4c767e6b53467f011c9f5548b2c8dbd25d5
Nyzr <nizarhamid80@gmail.com>
2022-04-06 23:01:47 +0800

    update controller

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -4,20 +4,23 @@
 <div class="container">
     <h1 class="mb-5">Subjects</h1>
     <div class="row gy-5">
+        @foreach ($subjects as $subject)
         <div class="col-4">
             <form action="{{ route('AddCart') }}" method="POST" > 
                 @csrf      
                 <div class="card" style="width: 18rem;">
                     <img src="..." class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5>{{ $test }}</h5>
-                        <h5 class="card-title">Subject Name</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h5 class="card-title">{{ $subject['title'] }}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Course: {{ $subject['courseCode'] }}</h6>
+                        <p class="card-text">Pages: {{ $subject['pages'] }}</p>
+                        <p class="card-text">Price: RM {{ $subject['price'] }}</p>
                         <button type="submit" class="btn btn-primary">Add To Cart</button>
                     </div>
                 </div>
             </form>
         </div>
+        @endforeach
     </div>
 </div>
 @endsection
