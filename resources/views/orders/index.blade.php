d099bf4453de85716ed9e537cb4e1c5d6dee753a
Nyzr <nizarhamid80@gmail.com>
2022-04-07 10:10:32 +0800

    update orderController and some views

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,25 +1,34 @@
 @extends('layouts.app')
-
+@php
+use App\Models\subject;
+@endphp
 @section('content')
 <div class="container">
     <h1 class="mb-5">Subjects</h1>
     <div class="row gy-5">
-        @foreach ($subjects as $subject)
-        <div class="col-4">
-            <form action="{{ route('AddCart') }}" method="POST" > 
-                @csrf      
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $subject['title'] }}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Course: {{ $subject['courseCode'] }}</h6>
-                        <p class="card-text">Pages: {{ $subject['pages'] }}</p>
-                        <p class="card-text">Price: RM {{ $subject['price'] }}</p>
-                        <button type="submit" class="btn btn-primary">Add To Cart</button>
+        @foreach ($subjectIDs as $subjectID)
+            @php
+                $subject = subject::where('subjectID',$subjectID->subjectID)
+                ->get();
+            @endphp
+            @foreach ($subject as $subjectDesc)
+            
+            <div class="col-4">
+                <form action="{{ route('AddCart') }}" method="POST" > 
+                    @csrf      
+                    <div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $subjectDesc['title'] }}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Course: {{ $subjectDesc['courseCode'] }}</h6>
+                            <p class="card-text">Pages: {{ $subjectDesc['pages'] }}</p>
+                            <p class="card-text">Price: RM {{ $subjectDesc['price'] }}</p>
+                            <button type="submit" class="btn btn-primary">Add To Cart</button>
+                        </div>
                     </div>
-                </div>
-            </form>
-        </div>
+                </form>
+            </div>
+            @endforeach
         @endforeach
     </div>
 </div>
