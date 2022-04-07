afcbc4544983d44a623e342cb91dbfb3f34dc45c
owen <chano-wm19@student.tarc.edu.my>
2022-04-08 02:13:35 +0800

    added xml but can't show

--- app/Http/Controllers/OrderHistoryController.php
+++ app/Http/Controllers/OrderHistoryController.php
@@ -11,28 +11,68 @@ class OrderHistoryController extends Controller
 {
     public function index()
     {
-
+        
         if(!auth()->user()){
             return view('users/orderHistory')->with('auth','0')->with('record',0);
         }
         
         $id = Auth::user()->id;
-
+        
         $data = $this->getOrderHistory($id);
-
+        
         if($data->isEmpty()){
             return view('users/orderHistory')->with('auth','1')->with('record',0);
         }else{
+            
+            $this->createXMLfile($data);
+
             return view('users/orderHistory')->with('auth','1')->with('record',1)->with('data',$data);
         }
         
         
     }
-
+    
     public function getOrderHistory($id){
         $orderData = DB::table('order')->where('userID',$id)->select('orderID','totalPrice','date','status','pickUpMethod')->get();
         
         return $orderData;
     }
+    
+    public function createXMLfile($datas){
+        $filePath = '../resources/XML/orderHistory.xml';
+        $dom     = new \DOMDocument('1.0', 'utf-8'); 
+        $dom->appendChild($dom->createProcessingInstruction('xml-stylesheet', 'type="text/xsl" href="orderHistories.xsl"'));
+        $root      = $dom->createElement('orderHistories'); 
+        
+        for($i=0; $i<count($datas); $i++){
+            
+            $orderID        =  $datas[$i]->orderID;  
+            $totalPrice     =  $datas[$i]->totalPrice;  
+            $date           =  $datas[$i]->date;  
+            $status         =  $datas[$i]->status;  
+            $pickUpMethod   =  $datas[$i]->pickUpMethod;
+            
+            $orderHistory = $dom->createElement('orderHistory');
 
+            $ID             =$dom->createElement('orderID', $orderID); 
+            $orderHistory->appendChild($ID);
+            
+            $ordertotalPrice  = $dom->createElement('totalPrice', $totalPrice); 
+            $orderHistory->appendChild($ordertotalPrice); 
+            
+            $orderDate   = $dom->createElement('date', $date); 
+            $orderHistory->appendChild($orderDate); 
+            
+            $orderStatus    = $dom->createElement('status', $status); 
+            $orderHistory->appendChild($orderStatus); 
+            
+            $orderMethod     = $dom->createElement('pickUpMethod', $pickUpMethod); 
+            $orderHistory->appendChild($orderMethod); 
+            
+            $root->appendChild($orderHistory);
+        }
+        $dom->appendChild($root); 
+        $dom->save($filePath); 
+        
+    }
 }
