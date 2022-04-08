c03207e58fa2a3a5a45e456d8251dc6cbad8d3e7
owen <chano-wm19@student.tarc.edu.my>
2022-04-08 21:47:35 +0800

    fixed routing

--- routes/web.php
+++ routes/web.php
@@ -130,7 +130,7 @@ Route::get('/programmedashboard', [ProgrammeController::class, 'retrieve'])->nam
 Route::get('/addsubject',[SubjectController::class, 'index']) -> name('addSubject');
 Route::post('/addsubject',[SubjectController::class,'store']);
 Route::get('/subjectdashboard', [SubjectController::class, 'retrieve'])->name('subjectdashboard');
-Route::get("/xmlSubject", [SubjectController::class,'viewInXml']);
+Route::get('/xmlSubject', [SubjectController::class,'viewInXml']);
 
 });
 
